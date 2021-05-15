7e132eedf33fc71b7292da191a9fe8408a8a318c
Дмитрий Демидов <dmitriidemidov@MacBook-Air-Dmitrij-2.local>
2021-05-15 13:46:07 +0300

    Missed file is added

--- tagcounter.py
+++ tagcounter.py
@@ -1,9 +1,29 @@
 import sys
 
-def get
+"""
+	Parsing of command line args to structured list.
+"""
+def parseCommnadLineArguments():
+	if (len(sys.argv) == 1):
+		print("No commands")
+		return {}
+
+	commands = {}
+	for i in range(1, len(sys.argv), 2):
+		if (i+1 < len(sys.argv)):
+			commands[sys.argv[i]] = sys.argv[i+1]
+			print(f"let's process {i} argument")
+
+	return commands
 
 def main():
 	print("hello mentoring")
-	print(f'number of arguments: {sys.argv}')
+
+	commands = parseCommnadLineArguments()
+	print(commands)
+	if len(commands) == 0:
+		print("Run GUI")
+	else:
+		print("run console")
 
 main()
